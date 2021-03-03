9d8a941d46a6a7c79ec7c0843eb9eed4f77485a8
constab1e <armenat3@gmail.com>
2021-03-03 15:26:50 +0300

    added fetching data from an api (json-server)

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,20 +1,26 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>Job id is {{ id }}</p>
-  <p>Job title is {{ title }}</p>
-  <p>Job details is {{ details }}</p>
+  <div v-if="job">
+    <h1>Job Details Page</h1>
+    <h1>Job title is {{ job.title }}</h1>
+    <p>Job id is {{ job.id }}</p>
+    <h3>{{ job.details }}</h3>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["id", "title", "details"],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //       title: this.$route.params.title,
-  //       details: this.$route.params.details,
-  //     };
-  //   },
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Jobs</h1>
-  
+
   <div v-for="job in jobs" :key="job.id" class="job">
     <router-link
       :to="{
@@ -17,12 +17,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "UX Designer", id: 1, details: "lorem" },
-        { title: "Web Developer", id: 2, details: "lorem" },
-        { title: "UX Vue Developer", id: 3, details: "lorem" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
